c8112cff4ab279e545ded27825d26e9b44e5b5ea
F1ynn Zhan9 <f1ynn_zhan9@foxmail.com>
2023-11-18 01:16:21 +0800

    initial commit

---
+++ src/main/java/com/example/leetspringboot/LeetSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.example.leetspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LeetSpringBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LeetSpringBootApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/leetspringboot/LeetSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.leetspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LeetSpringBootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
